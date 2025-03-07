9d631f9b19a212104637b7dd1cd85e7db62d5138
souryavarma <159093235+souryavarma@users.noreply.github.com>
2025-03-06 22:00:26 -0500

    updating the file

--- full_name.py
+++ full_name.py
@@ -1,8 +1,10 @@
 def fullName(firstName, lastName):
     return f"My Full Name is {firstName} {lastName}"
 
+
 def add(a, b):
     return a + b
+
 
 def multiple(a, b):
     return a * b
--- test_full_name.py
+++ test_full_name.py
@@ -4,19 +4,24 @@ from full_name import *
 def test_normal_case():
     assert fullName("John", "Doe") == "My Full Name is John Doe"
 
+
 def test_empty_strings():
     assert fullName("", "") == "My Full Name is  "
+
 
 def test_numbers_as_strings():
     assert fullName("123", "456") == "My Full Name is 123 456"
 
+
 def test_special_characters():
     assert fullName("@John", "#Doe") == "My Full Name is @John #Doe"
+
 
 def test_add_numbers():
     assert add(1, 2) == 3
 
+
 def test_mul_numbers():
-    assert multiple(2,2) == 4
+    assert multiple(2, 2) == 4
 
 
